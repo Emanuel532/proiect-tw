f47b8bf949f517ae4c068dcd8df9dcf7077307f4
Emanuel532 <ungureanuemanuelconstantin@yahoo.com>
2024-11-16 23:25:23 +0200

    enable signup/register functionality

--- skilltrade/skilltrade/settings.py
+++ skilltrade/skilltrade/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'skilltrade_aplicatie'
+    'skilltrade_aplicatie',
+    'accounts'
 ]
 
 MIDDLEWARE = [
