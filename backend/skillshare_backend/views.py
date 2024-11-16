9b8a4ec9a83097330c8a2fb2c1540af961169bc5
rares <raresstefea9@gmail.com>
2024-11-16 13:16:24 +0200

    Startup

--- backend/skillshare_backend/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-def index(request):
-    mesaj_test_django = "SALUT!"
-
-    data = {
-        'mesaj_django' : mesaj_test_django
-    }
-
-    return JsonResponse(data)
--- skilltrade/skilltrade/urls.py
+++ skilltrade/skilltrade/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for skillshare_backend project.
+URL configuration for skilltrade project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
 ]
